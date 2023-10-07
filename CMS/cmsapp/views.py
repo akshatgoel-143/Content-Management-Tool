e7d790d52666c31fcd0e7b1d07ced6674b74d14b
Akshat Goel <105805363+akshatgoel-143@users.noreply.github.com>
2023-10-07 23:33:51 +0530

    Add files via upload

---
+++ CMS/cmsapp/views.py
@@ -0,0 +1,65 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+# Create your views here.
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'cmsapp/home.html', context)
+
+
+def about(request):
+    return render(request, 'cmsapp/about.html', {'title':"About Page"})
+
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'cmsapp/home.html'
+    context_object_name = 'posts'
+    ordering = ["-date_posted"]
+    
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+        
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
